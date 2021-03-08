c342010ad766d90399450511d84cfabdf69f3e6c
Dimitris Kapoulas <dimkapoulas@hotmail.com>
2021-03-08 11:38:22 +0200

    Front end init. Homepage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 require('./bootstrap');
+
+// Import Vue package
+import Vue from 'vue'
+
+import App from './vue/app'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+var app = new Vue({
+    el: '#app',
+    components: { App }
+})
